8e4a6bd391d7e3ef734c6cde21e3189298e5d75b
Emmanuel Jimenez <emmanuel.jimenez@solvex.com.do>
2021-08-05 12:46:47 -0400

    initial

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,33 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+export enum MUTATIONS{
+  SET_USER_NAME = 'setUserName'
+}
+export enum ACTIONS{
+  SET_USER_NAME_ASYNC = 'setUserNameAsync'
+}
 export default new Vuex.Store({
   state: {
+    user:{
+      id:1,
+      userName:'emmanuel'
+    }
+  },
+  getters: {
+    getUserName(state){
+      return state.user.userName;
+    }
   },
   mutations: {
+    [MUTATIONS.SET_USER_NAME]: (state, userName) =>{
+      state.user.userName = userName;
+    },
   },
   actions: {
+    [ACTIONS.SET_USER_NAME_ASYNC](context,payload) {
+      new Promise(()=>context.commit(MUTATIONS.SET_USER_NAME,payload))
+    },
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    Username: {{ $store.getters.getUserName }}
+
+    <input type="text" v-model="userName" />
+    <button @click="changeUserName">Cambiar nombre de usuario</button>
+    <button @click="changeUserNameAsync">Cambiar nombre de usuario (async)</button>
+    <HelloWorld msg="Hola Mundo" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue, Watch } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import { ACTIONS, MUTATIONS } from "@/store";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  person = {
+    name: "Emmanuel",
+    lastName: "Jimenez",
+  };
+userName = this.$store.state.user.userName;
+
+  async changeUserNameAsync(){
+   await this.$store.dispatch(ACTIONS.SET_USER_NAME_ASYNC,this.userName);
+  }
+
+  changeUserName(){
+    this.$store.commit(MUTATIONS.SET_USER_NAME,this.userName);
+  }
+
+  @Watch("person", { deep: true })
+  onNameChange(newValue: { name: string; lastName: string }) {
+    console.log(newValue);
+
+  }
+}
 </script>
